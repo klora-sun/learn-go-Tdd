49fb1ac1ef2aa3e7dca96881156bfe970f05859b
SunYiDan <sunyidan@Regenovo.com>
2025-08-12 17:20:34 +0800

    feat(perimeter): add perimeter calculation function with tests
    Add Perimeter function to calculate rectangle perimeter from width and height.
    Include comprehensive test suite with helper function for result validation.

---
+++ perimeter/perimeter.go
@@ -0,0 +1,9 @@
+package Perimeter
+
+
+
+
+func Perimeter(width,height float64)float64{
+	result := width*2+height*2
+	return result
+}
--- perimeter/perimeter_test.go
+++ perimeter/perimeter_test.go
@@ -5,5 +5,20 @@ import "testing"
 
 func TestPerimeter(t *testing.T){
 
+	certainResult := func(t *testing.T, get, want float64) {
+		t.Helper()
+        if get != want {
+            t.Errorf("got '%f' want '%f'", get, want)
+        }
+	}
+
+	t.Run("test number",func(t *testing.T){
+		getWidth := 12
+		getHeight := 24
+		get := Perimeter(float64(getWidth),float64(getHeight))
+		want := 72
+		certainResult(t,get,float64(want))
+	})
+
 	
 }
